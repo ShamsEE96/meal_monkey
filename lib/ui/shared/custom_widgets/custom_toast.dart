9b40f32402356d0402049abc90fb5e7a0dd34450
ShamsEE96 <wolfsh96@gmail.com>
2023-04-28 18:03:42 +0300

    making a custom toast design adding message type enum to be used with toast

---
+++ lib/ui/shared/custom_widgets/custom_toast.dart
@@ -0,0 +1,80 @@
+import 'package:bot_toast/bot_toast.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:meal_monkey/data/enums/message_type.dart';
+import 'package:meal_monkey/ui/shared/colors.dart';
+import 'package:meal_monkey/ui/shared/extensions/custom_sized_box_shared.dart';
+
+class CustomToast {
+  static showMessage({
+    required Size size,
+    required String message,
+    MessageType? messageType = MessageType.INFO,
+  }) {
+    String imageName = 'toast_info';
+    Color shadowColor = AppColors.mainBlueColor;
+
+    switch (messageType) {
+      case MessageType.REJECTED:
+        imageName = 'toast_rejected';
+        shadowColor = AppColors.mainRedColor;
+        break;
+      case MessageType.SUCCESS:
+        imageName = 'toast_approved';
+        shadowColor = AppColors.mainGreenColor;
+        break;
+      case MessageType.WARNING:
+        imageName = 'toast_warning';
+        shadowColor = AppColors.mainOrangeColor;
+        break;
+      case MessageType.INFO:
+        imageName = 'toast_info';
+        shadowColor = AppColors.mainBlueColor;
+        break;
+      default:
+        break;
+    }
+
+    BotToast.showCustomText(
+      duration: Duration(seconds: 10),
+      toastBuilder: (value) {
+        return Container(
+          margin: EdgeInsets.all(size.width / 16),
+          width: size.width * 0.7,
+          // height: size.width * 0.4,
+          decoration: BoxDecoration(
+            color: AppColors.mainWhiteColor,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: shadowColor.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              // (size.height * 0.01).ph,
+              SvgPicture.asset(
+                'images/$imageName.svg',
+                height: size.width * 0.25,
+                width: size.width * 0.25,
+              ),
+              // (size.height * 0.01).ph,
+              Text(
+                message,
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: size.width * 0.065),
+              ),
+              // (size.height * 0.01).ph,
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
