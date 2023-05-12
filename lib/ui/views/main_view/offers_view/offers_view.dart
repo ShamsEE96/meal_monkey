9384377a2743c84807cafbcdf5a0fdc54756ebfd
ShamsEE96 <wolfsh96@gmail.com>
2023-05-12 11:58:25 +0300

    initial views for the BottomNavgationBar

---
+++ lib/ui/views/main_view/offers_view/offers_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class OffersView extends StatefulWidget {
+  const OffersView({super.key});
+
+  @override
+  State<OffersView> createState() => _OffersViewState();
+}
+
+class _OffersViewState extends State<OffersView> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        "Offers View",
+        style: TextStyle(
+          fontSize: 25,
+        ),
+      ),
+    );
+  }
+}
