58b5ef95e9623bcb1c969d705c3cb4f2e7f9afa5
ShamsEE96 <wolfsh96@gmail.com>
2023-05-20 15:01:27 +0300

    Added Endpoint folder Added all Endpoints needed for api

---
+++ lib/core/data/network/endpoints/cart_endpoints.dart
@@ -0,0 +1,3 @@
+import 'package:meal_monkey/core/data/network/network_config.dart';
+
+class CartEndpoints {}
---
+++ lib/core/data/network/endpoints/user_endpoints.dart
@@ -0,0 +1,5 @@
+import 'package:meal_monkey/core/data/network/network_config.dart';
+
+class UserEndpoints {
+  static String login = NetworkConfig.getFullApiRoute('user/login');
+}
