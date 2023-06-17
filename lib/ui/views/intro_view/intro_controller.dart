13591019c8801fc40d89342428ccb0e05c2105eb
ShamsEE96 <wolfsh96@gmail.com>
2023-06-18 00:49:17 +0300

    Applying Getx to intro_view still have some problems with translations

--- lib/ui/views/intro_view/intro_controller.dart
+++ lib/ui/views/intro_view/intro_controller.dart
@@ -1,16 +1,41 @@
 import 'package:get/get.dart';
 import 'package:meal_monkey/core/services/base_controller.dart';
+import 'package:meal_monkey/core/translation/app_translation.dart';
+import 'package:meal_monkey/ui/views/landing_view/landing_view.dart';
 
 class IntroController extends BaseController {
-  RxList<String> titleList = [
-    'Find Food You Love',
-    'Fast Delivery',
-    'Live Tracking',
-  ].obs;
-  RxList<String> descriptionList = [
-    'Discover the best foods from over 1,000 \n restaurants and fast delivery to your doorstep',
-    'Fast food delivery to your home, office \n wherever you are',
-    'Real time tracking of your food on the app \n once you placed the order,'
-  ].obs;
+  String title = tr('key_intro_title_01');
+  String description = tr('key_intro_description_01');
+  List<String> titleList = [
+    tr('key_intro_title_01'),
+    tr('key_intro_title_02'),
+    tr('key_intro_title_03'),
+  ];
+  List<String> descriptionList = [
+    tr('key_intro_description_01'),
+    tr('key_intro_description_02'),
+    tr('key_intro_description_03'),
+  ];
   RxInt currentIndex = 0.obs;
+
+  void nextIntro() {
+    if (currentIndex.value != 2) {
+      textList();
+      currentIndex.value += 1;
+    } else
+      Get.off(() => LandingView());
+  }
+
+  void textList() {
+    if (currentIndex.value == 0) {
+      title = tr('key_intro_title_01');
+      description = tr('key_intro_description_01');
+    } else if (currentIndex.value == 1) {
+      title = tr('key_intro_title_02');
+      description = tr('key_intro_description_02');
+    } else if (currentIndex.value == 2) {
+      title = tr('key_intro_title_03');
+      description = tr('key_intro_description_03');
+    }
+  }
 }
