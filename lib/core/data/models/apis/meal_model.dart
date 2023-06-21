c9b449671ee970ae993ddfc122fec39033d5dd65
ShamsEE96 <wolfsh96@gmail.com>
2023-06-21 19:39:19 +0300

    Fixed MealModel was missing discreption

--- lib/core/data/models/apis/meal_model.dart
+++ lib/core/data/models/apis/meal_model.dart
@@ -1,26 +1,39 @@
 class MealModel {
+  int? id;
   String? name;
-  int? mealId;
-  int? categoryId;
+  String? categoryName;
   int? price;
+  int? categoryId;
+  String? description;
   List<String>? images;
 
-  MealModel({this.name, this.mealId, this.categoryId, this.price, this.images});
+  MealModel(
+      {this.id,
+      this.name,
+      this.categoryName,
+      this.price,
+      this.categoryId,
+      this.description,
+      this.images});
 
   MealModel.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
     name = json['name'];
-    mealId = json['mealId'];
-    categoryId = json['categoryId'];
+    categoryName = json['categoryName'];
     price = json['price'];
+    categoryId = json['categoryId'];
+    description = json['description'];
     images = json['images'].cast<String>();
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
     data['name'] = this.name;
-    data['mealId'] = this.mealId;
-    data['categoryId'] = this.categoryId;
+    data['categoryName'] = this.categoryName;
     data['price'] = this.price;
+    data['categoryId'] = this.categoryId;
+    data['description'] = this.description;
     data['images'] = this.images;
     return data;
   }
