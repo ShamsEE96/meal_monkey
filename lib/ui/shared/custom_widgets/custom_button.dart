81496f9ab59fa4c0827b86171a17a0498fcc1318
ShamsEE96 <wolfsh96@gmail.com>
2023-06-25 11:20:10 +0300

    Added more parameters to CustomButton Changed the size from Fixed Size to wrap with SizedBox

--- lib/ui/shared/custom_widgets/custom_button.dart
+++ lib/ui/shared/custom_widgets/custom_button.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:meal_monkey/ui/shared/colors.dart';
+import 'package:meal_monkey/ui/shared/custom_widgets/custom_text.dart';
 import 'package:meal_monkey/ui/shared/extensions/custom_sized_box_shared.dart';
 import 'package:meal_monkey/ui/shared/utils.dart';
 
@@ -12,54 +13,72 @@ class CustomButton extends StatelessWidget {
     this.backgroundColor,
     this.borderColor,
     required this.onPressed,
-    this.ImageName,
+    this.imageName,
     this.width,
     this.height,
+    this.imageColor,
+    this.imageWidth,
+    this.imageHeight,
+    this.fontWeight,
+    this.fontSize,
   });
 
   final String? text;
-  final String? ImageName;
+  final String? imageName;
+  final Color? imageColor;
   final Color? borderColor;
   final Color? textColor;
   final Color? backgroundColor;
   final Function? onPressed;
   final double? width;
   final double? height;
+  final double? imageWidth;
+  final double? imageHeight;
+  final double? fontSize;
+  final FontWeight? fontWeight;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {
-        if (onPressed != null) onPressed!();
-      },
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          if (ImageName != null) ...[
-            SvgPicture.asset('assets/images/$ImageName.svg'),
-            (screenWidth(20)).pw,
-          ],
-          Text(
-            text ?? "",
-            style: TextStyle(
-              color: textColor,
-              fontWeight: FontWeight.bold,
+    return SizedBox(
+      width: width ?? screenWidth(1.1),
+      height: height ?? screenHeight(12),
+      child: ElevatedButton(
+        onPressed: () {
+          if (onPressed != null) onPressed!();
+        },
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (imageName != null) ...[
+              SvgPicture.asset(
+                'assets/images/$imageName.svg',
+                color: imageColor,
+                width: imageWidth,
+                height: imageHeight,
+              ),
+              (screenWidth(20)).pw,
+            ],
+            CustomText(
+              text: text ?? "",
+              textColor: textColor,
+              fontWeight: fontWeight ?? FontWeight.bold,
+              fontSize: fontSize ?? screenWidth(25),
             ),
-          ),
-        ],
-      ),
-      style: ElevatedButton.styleFrom(
-        side: borderColor != null
-            ? BorderSide(
-                width: 1.0,
-                color: borderColor!,
-              )
-            : null,
-        backgroundColor: backgroundColor ?? AppColors.mainOrangeColor,
-        shape: StadiumBorder(),
-        fixedSize: Size(
-          width ?? screenWidth(1.1),
-          height ?? screenHeight(12),
+          ],
+        ),
+        style: ElevatedButton.styleFrom(
+          side: borderColor != null
+              ? BorderSide(
+                  width: 1.0,
+                  color: borderColor!,
+                )
+              : null,
+          backgroundColor: backgroundColor ?? AppColors.mainOrangeColor,
+          shape: StadiumBorder(),
+          // fixedSize: Size(
+          //   width ?? screenWidth(1.1),
+          //   height ?? screenHeight(12),
+          // ),
         ),
       ),
     );
