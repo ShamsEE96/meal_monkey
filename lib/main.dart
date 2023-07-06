6eb1984f01c590cc720569f38dfc97a7b3642ac0
ShamsEE96 <wolfsh96@gmail.com>
2023-07-06 17:46:55 +0300

    Added Dependency injection for CartService

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:meal_monkey/app/my_app.dart';
 import 'package:meal_monkey/core/data/repositories/shared_preferences_repository.dart';
+import 'package:meal_monkey/core/services/cart_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Future<void> main() async {
@@ -10,6 +11,7 @@ Future<void> main() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs;
   });
-  Get.put(SharedPreferencesRepository());
+  await Get.put(SharedPreferencesRepository());
+  Get.put(CartService());
   runApp(const MyApp());
 }
